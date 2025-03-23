d0c7768924e6c517e4a9b62fb621cda965f7260e
Dmitry Goldenberg <epuckop@gmail.com>
2025-03-23 18:09:09 +0200

    testing azurerm_management_group

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # tfc_azure_lz_core
 Deploy the core infrastructure for the Azure landing zone using Terraform Cloud.
+
+## Prerequisites
+
+- Terraform Cloud account
+- Azure tenant with full admin permissions
+- Azure AD application with configured federation and all necessary permissions
--- main.tf
+++ main.tf
@@ -1,4 +1,16 @@
-resource "azurerm_resource_group" "example" {
-  name     = "example-resource-group"
-  location = "israelcentral"
+data "azurerm_subscription" "current" {
+}
+
+resource "azurerm_management_group" "example_parent" {
+  display_name = "example mng group"
+}
+
+resource "azurerm_management_group" "example_child" {
+  display_name               = "Child Group"
+  parent_management_group_id = azurerm_management_group.example_parent.id
+
+  subscription_ids = [
+    data.azurerm_subscription.current.subscription_id,
+  ]
+  # other subscription IDs can go here
 }
