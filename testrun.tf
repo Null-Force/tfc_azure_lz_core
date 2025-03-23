e46846ae8cf3ca80c2028adce32c7c477ee3499a
Dmitry Goldenberg <epuckop@gmail.com>
2025-03-23 17:17:07 +0200

    test run 2

--- testrun.tf
+++ testrun.tf
@@ -3,3 +3,8 @@ data "azurerm_client_config" "current" {}
 output "client_id" {
   value = data.azurerm_client_config.current.client_id
 }
+
+resource "azurerm_resource_group" "example" {
+  name     = "example-resource-group"
+  location = "israelcentral"
+}
