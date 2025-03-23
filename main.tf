fa53010993625a3a58c484ac79c53497ecee03c2
Dmitry Goldenberg <epuckop@gmail.com>
2025-03-23 18:12:04 +0200

    update sub

--- main.tf
+++ main.tf
@@ -8,9 +8,5 @@ resource "azurerm_management_group" "example_parent" {
 resource "azurerm_management_group" "example_child" {
   display_name               = "Child Group"
   parent_management_group_id = azurerm_management_group.example_parent.id
-
-  subscription_ids = [
-    data.azurerm_subscription.current.subscription_id,
-  ]
   # other subscription IDs can go here
 }
