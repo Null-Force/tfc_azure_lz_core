67f68f12ef9b679566e4af3fc04315b1245e66f2
Dmitry Goldenberg <epuckop@gmail.com>
2025-04-05 22:36:28 +0300

    add some comments and validations

--- main.tf
+++ main.tf
@@ -1,11 +1,14 @@
-resource "azurerm_management_group" "parent" {  
+# Creage a management groups
+# New root management group
+resource "azurerm_management_group" "parent" {
   display_name = local.parent_mng_group_name
 }
 
+# L1 management groups
 resource "azurerm_management_group" "mng_groups_l1" {
   for_each = local.mng_groups_parsed_l1
 
-  display_name = each.key
+  display_name               = each.key
   parent_management_group_id = azurerm_management_group.parent.id
 
   lifecycle {
@@ -13,10 +16,11 @@ resource "azurerm_management_group" "mng_groups_l1" {
   }
 }
 
+# L2 management groups
 resource "azurerm_management_group" "mng_groups_l2" {
   for_each = local.mng_groups_parsed_l2
 
-  display_name = each.value.name
+  display_name               = each.value.name
   parent_management_group_id = azurerm_management_group.mng_groups_l1[each.value.group].id
 
   lifecycle {
--- variables.tf
+++ variables.tf
@@ -2,10 +2,20 @@ variable "parent_mng_group_name" {
   description = "The names of the parent management groups"
   type        = string
   default     = "null_power"
+
+  validation {
+    condition     = length(var.parent_mng_group_name) >= 4 && length(var.parent_mng_group_name) <= 30
+    error_message = "The parent management group name must be between 4 and 30 characters."
+  }
 }
 
 variable "mng_groups_names" {
   description = "The names of the management groups"
   type        = string
   default     = "{\"platform\":[\"management\",\"identity\",\"connectivity\"],\"landing_zones\":[\"corporate\",\"online\"],\"decommissioned\":[],\"sandbox\":[]}"
+
+  validation {
+    condition     = can(jsondecode(var.mng_groups_names))
+    error_message = "The management group names must be a valid JSON string. Default value is: {\"platform\":[\"management\",\"identity\",\"connectivity\"],\"landing_zones\":[\"corporate\",\"online\"],\"decommissioned\":[],\"sandbox\":[]}"
+  }
 }
