7482ae04d70b37c823c42c14de159ad12bcdecd6
Dmitry Goldenberg <epuckop@gmail.com>
2025-04-05 21:51:48 +0300

    testing l2

--- main.tf
+++ main.tf
@@ -20,16 +20,17 @@ locals {
 
   mng_groups_parsed_l1 = jsondecode(var.mng_groups_names)
 
-  mng_groups_parsed_l2 = {
+  mng_groups_parsed_l2 = merge([
     for group, names in local.mng_groups_parsed_l1 :
-    group => [
-      for name in names : {
+    {
+      for name in names :
+      "${group}_${name}" => {
         name  = name
         group = group
       }
-    ]
+    }
     if length(names) > 0
-  }
+  ]...)
 }
 
 output "names_l2" {
