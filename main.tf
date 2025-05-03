2124d43482c79dfb3dcf6cff7a9554a5f3cd5a0e
Dmitry Goldenberg <epuckop@gmail.com>
2025-05-04 00:11:25 +0300

    feat: add Azure AD group resource for management group owners

--- main.tf
+++ main.tf
@@ -20,3 +20,20 @@ resource "azurerm_management_group" "second_level" {
   display_name               = each.value.name
   parent_management_group_id = azurerm_management_group.first_level[each.value.parent].id
 }
+
+# Create AD groups
+
+
+resource "azuread_group" "owners" {
+  for_each = merge(
+    azurerm_management_group.first_level,
+    azurerm_management_group.second_level
+  )
+
+  display_name            = "${each.value.display_name} - owners"
+  owners                  = [data.azuread_client_config.current.object_id]
+  security_enabled        = true
+  assignable_to_role      = true
+  description             = "Owners group for ${each.value.display_name} management group"
+  prevent_duplicate_names = false
+}
