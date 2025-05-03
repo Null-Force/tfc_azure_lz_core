e3311f9d9bb61fa73807cb315d0693496823f243
Dmitry Goldenberg <epuckop@gmail.com>
2025-05-03 17:11:36 +0300

    update groups deployment method

--- variables.tf
+++ variables.tf
@@ -8,14 +8,3 @@ variable "parent_mng_group_name" {
     error_message = "The parent management group name must be between 4 and 30 characters."
   }
 }
-
-variable "mng_groups_names" {
-  description = "The names of the management groups"
-  type        = string
-  default     = "{\"platform\":[\"management\",\"identity\",\"connectivity\"],\"landing_zones\":[\"corporate\",\"online\"],\"decommissioned\":[],\"sandbox\":[]}"
-
-  validation {
-    condition     = can(jsondecode(var.mng_groups_names))
-    error_message = "The management group names must be a valid JSON string. Default value is: {\"platform\":[\"management\",\"identity\",\"connectivity\"],\"landing_zones\":[\"corporate\",\"online\"],\"decommissioned\":[],\"sandbox\":[]}"
-  }
-}
