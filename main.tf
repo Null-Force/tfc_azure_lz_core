c1c2a424a44051911a5d7fe14a4eafd902246fbe
Dmitry Goldenberg <epuckop@gmail.com>
2025-05-04 00:28:16 +0300

    feat: add contributors and readers groups for management groups

--- main.tf
+++ main.tf
@@ -21,9 +21,9 @@ resource "azurerm_management_group" "second_level" {
   parent_management_group_id = azurerm_management_group.first_level[each.value.parent].id
 }
 
-# Create AD groups
+# Create Entra ID securety groups for each management group with owners, contributer and reader roles
 
-
+## Create the owners group for each management group
 resource "azuread_group" "owners" {
   for_each = merge(
     azurerm_management_group.first_level,
@@ -46,4 +46,52 @@ resource "azurerm_role_assignment" "owners" {
   scope                = each.value.id
   role_definition_name = "Owner"
   principal_id         = azuread_group.owners[each.key].object_id
+}
+
+## Create the contributors group for each management group
+resource "azuread_group" "contributors" {
+  for_each = merge(
+    azurerm_management_group.first_level,
+    azurerm_management_group.second_level
+  )
+
+  display_name            = "management group - ${each.value.display_name} - contributors"
+  owners                  = [data.azuread_client_config.current.object_id]
+  security_enabled        = true
+  description             = "Contributors group for ${each.value.display_name} management group"
+  prevent_duplicate_names = false
+}
+resource "azurerm_role_assignment" "contributors" {
+  for_each = merge(
+    azurerm_management_group.first_level,
+    azurerm_management_group.second_level
+  )
+
+  scope                = each.value.id
+  role_definition_name = "Contributor"
+  principal_id         = azuread_group.contributors[each.key].object_id
+}
+
+## Create the readers group for each management group
+resource "azuread_group" "readers" {
+  for_each = merge(
+    azurerm_management_group.first_level,
+    azurerm_management_group.second_level
+  )
+
+  display_name            = "management group - ${each.value.display_name} - readers"
+  owners                  = [data.azuread_client_config.current.object_id]
+  security_enabled        = true
+  description             = "Readers group for ${each.value.display_name} management group"
+  prevent_duplicate_names = false
+}
+resource "azurerm_role_assignment" "readers" {
+  for_each = merge(
+    azurerm_management_group.first_level,
+    azurerm_management_group.second_level
+  )
+
+  scope                = each.value.id
+  role_definition_name = "Reader"
+  principal_id         = azuread_group.readers[each.key].object_id
 }
