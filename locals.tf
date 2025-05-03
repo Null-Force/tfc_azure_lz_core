a90b1d5983d4d374f67de878e1ad11c58d2d580e
Dmitry Goldenberg <epuckop@gmail.com>
2025-05-03 22:05:39 +0300

    update group deploy method - tst run 1

--- locals.tf
+++ locals.tf
@@ -1,3 +1,26 @@
 locals {
   parent_mng_group_name = var.parent_mng_group_name
+
+
+  # The names of the management groups
+  management_groups_tree = jsondecode(file("${path.module}/config/management_groups_tree.json"))
+
+  # First level management group
+  first_level_mng_group_map = {
+    for key, value in local.management_groups_tree :
+    key => {
+      name = value.name
+    }
+  }
+
+  # Second level management group
+  second_level_mng_group_map = merge([
+    for root_key, root_value in local.management_groups_tree : {
+      for key, value in root_value.children :
+      key => {
+        name   = value.name
+        parent = root_key
+      } if length(root_value.children) > 0
+    } if length(root_value.children) > 0
+  ]...)
 }
--- main.tf
+++ main.tf
@@ -1,5 +1,22 @@
 # Creage a management groups
-# New root management group
+
+## New root management group
 resource "azurerm_management_group" "parent" {
   display_name = local.parent_mng_group_name
+}
+
+## Create the first level management groups
+resource "azurerm_management_group" "first_level" {
+  for_each = local.first_level_mng_group_map
+
+  display_name               = each.value.name
+  parent_management_group_id = azurerm_management_group.parent.id
+}
+
+## Create the second level management groups
+resource "azurerm_management_group" "second_level" {
+  for_each = local.second_level_mng_group_map
+
+  display_name               = each.value.name
+  parent_management_group_id = azurerm_management_group.first_level[each.value.parent].id
 }
